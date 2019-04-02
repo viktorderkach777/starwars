ea16112ed41dbc0771ff4715fd1b7a09eec99201
viktorderkach777 <viktorderkach777@yandex.ua>
2019-04-02 21:14:56 +0300

    add planets

--- src/App.js
+++ src/App.js
@@ -1,99 +1,175 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import './App.css';
 import 'bulma/css/bulma.css';
 
 
 class WeatherService extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      weatherData : null,
+      weatherData: null,
     }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     const zip = this.props.zip;
-    const apiURL = "http://api.openweathermap.org/data/2.5/weather?q=" +
+    //     const apiURL = "http://api.openweathermap.org/data/2.5/weather?q=" +
 
-     zip +
+    //      zip +
 
-     "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=metric";
-fetch(apiURL)
-.then( res => res.json())
-.then(json=> {this.setState( {weatherData: json})})
+    //      "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=metric";
+    const apiURL = "https://swapi.co/api/planets/?page=" + zip;
+
+    fetch(apiURL)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          weatherData: json
+        })
+      })
   }
 
   render() {
     const weatherData = this.state.weatherData;
-
-    if(!weatherData){
-return(
-  <div>Loading data...</div>
-);
-    }
-
-    const weather = weatherData.weather[0];
-const iconUrl = "http://api.openweathermap.org/img/w/" + weather.icon + ".png";
-
+    if (!weatherData) return <div> Loading data... </div>
+    
+    const planets = weatherData.results;
     return (
-      // <div>
-      // <h1>Hello from OpenWeatherApi!!!</h1>   
-      // <p>Weather for city with zipCode = { this.props.zip }</p>   
-      // </div>
-<div>
-  <h1>
-{weather.main} in {weatherData.name}
-<img src={iconUrl} alt={weatherData.description}/>
-  </h1>
-  <p>Current temp: {weatherData.main.temp}</p>
-  <p>High temp: {weatherData.main.temp_max}</p>
-  <p>Low temp: {weatherData.main.temp_min}</p>
-  <p>Pressure: {weatherData.main.pressure}</p>
-  <p>Humidity: {weatherData.main.humidity}</p>
-  <p>Wind speed: {weatherData.wind.speed } m/s</p>
-  <p>Wind direction: {weatherData.wind.deg }</p>
-  {/* { JSON.stringify(weatherData)} */}
-</div>
-     
+      <div>
+        {
+          planets.map((planet, index)=>  <p>Name:{planet.name}</p>)
+        }
+      </div>
+
+      // weatherData.map(
+      //   (index)=>(
+      //     <p>Name:{weatherData.results[index].name}</p>
+      //   )
+      // )
+
+      // <
+      // div >
+
+      // {
+      //   JSON.stringify(weatherData)
+      // } {
+      //   console.log(JSON.stringify(weatherData.results[0].name))
+      // } <
+      // p > Name: {
+      //   weatherData.results[0].name
+      // } < /p>
+
+      // <
+      // p > Diameter: {
+      //   weatherData.results[0].diameter
+      // } < /p>
+
+      // <
+      // p > Rotation_period: {
+      //   weatherData.results[0].rotation_period
+      // } < /p>
+
+      // <
+      // p > Orbital_period: {
+      //   weatherData.results[0].orbital_period
+      // } < /p>
+
+      // <
+      // p > Gravity: {
+      //   weatherData.results[0].gravity
+      // } < /p>
+
+      // <
+      // p > Population: {
+      //   weatherData.results[0].population
+      // } < /p>
+
+      // <
+      // p > Climate: {
+      //   weatherData.results[0].climate
+      // } < /p>
+
+      // <
+      // p > Terrain: {
+      //   weatherData.results[0].terrain
+      // } < /p>
+
+      // <
+      // p > Surface_water: {
+      //   weatherData.results[0].surface_water
+      // } < /p> <
+      // /div>
+
     );
   }
 }
 
-const PLACES = [
-{name:"Washington", zip: "10001"},
-{name:"New York", zip: "20001"},
-{name:"Seatle", zip: "30001"},
-{name:"Las Vegas", zip: "40001"},
+// const PLACES = [
+// {name:"Washington", zip: "10001"},
+// {name:"New York", zip: "20001"},
+// {name:"Seatle", zip: "30001"},
+// {name:"Las Vegas", zip: "40001"},
+// ];
+
+const PLACES = [{
+    zip: "1"
+  },
+  {
+    zip: "2"
+  },
+  {
+    zip: "3"
+  },
+  {
+    zip: "4"
+  },
 ];
 
+
+
+
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      activePlace : 0,
+      activePlace: 0,
     }
   }
 
   render() {
     const activePlace = this.state.activePlace;
-    return (
-      <div className="App">
-       {/* <p> Hello world from react.JS!! </p> */}
-       {
-         PLACES.map( (place, index)=> (
-           <button key={index}
-            className={"button " + (index%2===0?"is-danger":"is-primary")              
-        }
-           onClick={() => {
-            //  console.log(`Button ${index} clicked!!!`)
-            this.setState({activePlace: index})
-             }}>
-           {place.name}
-           </button>
-         ))
-       }
-       <WeatherService zip={ PLACES[activePlace].zip } key={activePlace}></WeatherService>
-      </div>
+    return ( 
+      <div className = "App" > {
+        /* <p> Hello world from react.JS!! </p> */ } {
+        PLACES.map((place, index) => ( 
+          <button key = {
+            index
+          }
+          className = {
+            "button " + (index % 2 === 0 ? "is-danger" : "is-primary")
+          }
+          onClick = {
+            () => {
+              //  console.log(`Button ${index} clicked!!!`)
+              this.setState({
+                activePlace: index
+              })
+            }
+          } > {
+            place.zip
+          } <
+          /button>
+        ))
+      } <
+      WeatherService zip = {
+        PLACES[activePlace].zip
+      }
+      key = {
+        activePlace
+      } > < /WeatherService> <
+      /div>
     );
   }
 }
